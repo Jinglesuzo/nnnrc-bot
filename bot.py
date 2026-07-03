c64fb9a618eb5b05cf032ff758f94cb9cc923563
Jinglesuzo <146891843+Jinglesuzo@users.noreply.github.com>
2026-07-03 16:29:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -481,5 +481,5 @@ class NigerianAccountBot:
 target_url = "https://nnnrc.com/#/register"
 NUM_ACCOUNTS = 3
 
-bot = NigerianAccountBot(start_code=102526)
+bot = NigerianAccountBot(start_code=102537)
 bot.run(target_url, num_accounts=NUM_ACCOUNTS)
